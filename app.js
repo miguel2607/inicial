a33238d6c788f30bda8b03ef87e4702a9e8bade2
Miguel <marodriguez_141@cue.edu.co>
2024-02-08 14:55:11 -0500

    Se crea el archivo app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+ //console.log("Hola mundo")
+ let name = "Miguel Angel"
+ let age = 17
+
+ //Aqui imprimimos el tipo de variable de age
+ console.log("La variable age es de tipo: " + typeof(age))
+ console.log("La variable name es de tipo: " + typeof(name))
