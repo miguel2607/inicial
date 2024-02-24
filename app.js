0b3ab268c8682d00b427a9c71220e271fb682543
miguel rodriguez <158486087+miguel2607@users.noreply.github.com>
2024-02-24 15:48:30 -0500

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
  //console.log("Hola mundo")
  let name = "Miguel Angel"
  let age = 17
-
+let mensage = "hello" 
  //Aqui imprimimos el tipo de variable de age
  console.log("La variable age es de tipo: " + typeof(age))
  console.log("La variable name es de tipo: " + typeof(name))    
+console.log(" la variable mensage es de tipo:" + scrip (mensage)
